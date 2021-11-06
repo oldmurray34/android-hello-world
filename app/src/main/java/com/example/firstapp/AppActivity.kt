59b5255172da5fc8bef7c8b6bd2c3ad520b8588f
oldmurray34 <oldmurray34@gmail.com>
2021-11-06 10:40:44 +0300

    Added notifications

--- app/src/main/java/com/example/firstapp/AppActivity.kt
+++ app/src/main/java/com/example/firstapp/AppActivity.kt
@@ -2,9 +2,14 @@ package com.example.firstapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.example.firstapp.NewPostFragment.Companion.contentArg
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.api.GoogleApiActivity
+import com.google.firebase.messaging.FirebaseMessaging
 
 class AppActivity : AppCompatActivity(R.layout.activity_app) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +28,26 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
                     })
                 return@let
             }
+        }
+        checkGoogleApiAvailability()
+    }
+
+    private fun checkGoogleApiAvailability() {
+        with(GoogleApiAvailability.getInstance()) {
+            val code = isGooglePlayServicesAvailable(this@AppActivity)
+            if (code == ConnectionResult.SUCCESS) {
+                return@with
+            }
+            if (isUserResolvableError(code)) {
+                getErrorDialog(this@AppActivity, code, 9000).show()
+                return
+            }
+            Toast.makeText(this@AppActivity, "Google Api Unavailable", Toast.LENGTH_LONG)
+                .show()
+        }
+
+        FirebaseMessaging.getInstance().token.addOnSuccessListener {
+            println(it)
         }
     }
 }
---
+++ app/src/main/java/com/example/firstapp/service/FCMService.kt
@@ -0,0 +1,102 @@
+package com.example.firstapp.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.firstapp.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
+import kotlin.random.Random
+
+
+class FCMService : FirebaseMessagingService() {
+    private val action = "action"
+    private val content = "content"
+    private val channelId = "remote"
+    private val gson = Gson()
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_remote_name)
+            val descriptionText = getString(R.string.channel_remote_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+
+        if (message.data[action] == "LIKE") {
+            handleLike(gson.fromJson(message.data[content], Like::class.java))
+        } else if (message.data[action] == "NEW_POST") {
+            handleNewPost(gson.fromJson(message.data[content], NewPost::class.java))
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        println(token)
+    }
+
+    private fun handleLike(content: Like) {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.notification_user_liked,
+                    content.userName,
+                    content.postAuthor,
+                )
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        NotificationManagerCompat.from(this)
+            .notify(Random.nextInt(100_000), notification)
+    }
+
+    private fun handleNewPost(content: NewPost) {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.notification_new_post,
+                    content.userName,
+                )
+            )
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText(content.postContent))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        NotificationManagerCompat.from(this)
+            .notify(Random.nextInt(100_000), notification)
+    }
+}
+
+enum class Action {
+    LIKE,
+    NEW_POST
+}
+
+data class Like(
+    val userId: Long,
+    val userName: String,
+    val postId: Long,
+    val postAuthor: String,
+)
+
+data class NewPost(
+    val userId: Long,
+    val userName: String,
+    val postId: Long,
+    val postContent: String
+)
